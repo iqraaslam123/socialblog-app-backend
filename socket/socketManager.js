ab1e33b701b53e4ab51636a082a5f3e91c84d810
iqraaslam123 <iqraaslam1966@gmail.com>
2026-05-30 17:23:12 +0500

    new

--- socket/socketManager.js
+++ socket/socketManager.js
@@ -40,9 +40,44 @@
 // module.exports = { socketManager };
 
 
-import { io } from "socket.io-client";
+const socketManager = (io) => {
+  const onlineUsers = new Map();
 
-const socket = io("https://socialblog-app-backend.vercel.app", {
-  transports: ["polling"], // 👈 Yeh line zaroor add karein agar backend Vercel par rakhna hai
-  withCredentials: true
-});
+  io.on('connection', (socket) => {
+    socket.on('user_connected', (userId) => {
+      onlineUsers.set(userId, socket.id);
+      io.emit('online_users', [...onlineUsers.keys()]);
+    });
+
+    socket.on('send_message', (message) => {
+      const receiverSocket = onlineUsers.get(message.receiver._id || message.receiver);
+      if (receiverSocket) io.to(receiverSocket).emit('receive_message', message);
+    });
+
+    socket.on('typing', ({ to, from }) => {
+      const receiverSocket = onlineUsers.get(to);
+      if (receiverSocket) io.to(receiverSocket).emit('typing', { from });
+    });
+
+    socket.on('stop_typing', ({ to }) => {
+      const receiverSocket = onlineUsers.get(to);
+      if (receiverSocket) io.to(receiverSocket).emit('stop_typing');
+    });
+
+    // Notifications
+    socket.on('send_notification', ({ toUserId, notification }) => {
+      const receiverSocket = onlineUsers.get(toUserId);
+      if (receiverSocket) io.to(receiverSocket).emit('notification', notification);
+    });
+
+    socket.on('disconnect', () => {
+      for (const [userId, socketId] of onlineUsers.entries()) {
+        if (socketId === socket.id) { onlineUsers.delete(userId); break; }
+      }
+      io.emit('online_users', [...onlineUsers.keys()]);
+    });
+  });
+};
+
+// Yeh CommonJS ka tarika hai export karne ka
+module.exports = { socketManager };
